93568da54fb86355d61015f83aa7be0e8b42e395
FrolDy <78847618+FrolDy@users.noreply.github.com>
2021-02-14 14:15:17 +0300

    Create СmakeProject1.cpp

---
+++ СmakeProject1.cpp
@@ -0,0 +1,27 @@
+#include "CMakeProject1.h"
+#include <fstream>
+#define S_RATE  (44100)
+#define BUF_SIZE (S_RATE*2)
+using namespace std;
+
+int main()
+{
+	int i;
+	float t;
+	int16_t buffer[BUF_SIZE];
+	float amplitude = 32000;
+	
+	float freq_Hz = 1000;
+	float phase = 0;
+	ofstream test;
+	test.open("D:\\test.pcm");
+	for (i = 0; i < BUF_SIZE; i++)
+	{
+		
+		buffer[i] = (int)(amplitude * sin((float)(2 * 3.14 * i * freq_Hz / S_RATE)));
+		test << buffer[i];
+		cout << buffer[i] << endl;
+	}
+	
+	return 0;
+}
